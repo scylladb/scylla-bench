1acb21b44f4df294ece229059c0b0c39d16f99fa
Paweł Dziepak <pdziepak@scylladb.com>
2017-05-22 15:21:59 +0100

    add test for uniform workload

--- workloads_test.go
+++ workloads_test.go
@@ -75,3 +75,45 @@ func TestSequentialWorkload(t *testing.T) {
 		})
 	}
 }
+
+func TestUniformWorkload(t *testing.T) {
+	generator := rand.New(rand.NewSource(int64(time.Now().Nanosecond())))
+	testCases := []struct {
+		partitionCount     int
+		clusteringRowCount int
+	}{
+		{20, 30},
+		{1, 1},
+		{generator.Intn(100) + 100, generator.Intn(99) + 1},
+		{generator.Intn(100), generator.Intn(100)},
+		{generator.Intn(100) + 100, 1},
+		{generator.Intn(100) + 100, generator.Intn(99) + 1},
+		{generator.Intn(100) + 100, generator.Intn(99) + 1},
+	}
+
+	for i, tc := range testCases {
+		t.Run(fmt.Sprintf("rand%d", i), func(t *testing.T) {
+			wrkld := NewRandomUniform(i, tc.partitionCount, tc.clusteringRowCount)
+
+			for i := 0; i < 1000; i++ {
+				if wrkld.IsDone() {
+					t.Error("got end of stream")
+				}
+
+				pk := wrkld.NextPartitionKey()
+				if pk < 0 || pk >= tc.partitionCount {
+					t.Errorf("PK %d out of range: [0-%d)", pk, tc.partitionCount)
+				}
+
+				ck := wrkld.NextClusteringKey()
+				if ck < 0 || ck >= tc.clusteringRowCount {
+					t.Errorf("CK %d out of range: [0-%d)", pk, tc.clusteringRowCount)
+				}
+
+				if wrkld.IsPartitionDone() {
+					t.Error("got end of partition")
+				}
+			}
+		})
+	}
+}
