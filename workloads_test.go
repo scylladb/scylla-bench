3ac92511d22244ef238f59943363418b51c0b8c9
Paweł Dziepak <pdziepak@scylladb.com>
2017-05-22 15:16:37 +0100

    add test for sequential workload

---
+++ workloads_test.go
@@ -0,0 +1,77 @@
+package main
+
+import (
+	"fmt"
+	"math/rand"
+	"testing"
+	"time"
+)
+
+func TestSequentialWorkload(t *testing.T) {
+	generator := rand.New(rand.NewSource(int64(time.Now().Nanosecond())))
+	testCases := []struct {
+		partitionOffset    int
+		partitionCount     int
+		clusteringRowCount int
+	}{
+		{10, 20, 30},
+		{0, 1, 1},
+		{generator.Intn(100), generator.Intn(100) + 100, generator.Intn(99) + 1},
+		{generator.Intn(100), generator.Intn(100), generator.Intn(100)},
+		{generator.Intn(100), generator.Intn(100) + 100, 1},
+		{0, generator.Intn(100) + 100, generator.Intn(99) + 1},
+		{0, generator.Intn(100) + 100, generator.Intn(99) + 1},
+	}
+
+	for i, tc := range testCases {
+		t.Run(fmt.Sprintf("rand%d", i), func(t *testing.T) {
+			wrkld := NewSequentialVisitAll(tc.partitionOffset, tc.partitionCount, tc.clusteringRowCount)
+
+			expectedPk := tc.partitionOffset
+			expectedCk := 0
+
+			for true {
+				if wrkld.IsDone() && expectedPk < tc.partitionOffset+tc.partitionCount {
+					t.Errorf("got end of stream; expected %d", expectedPk)
+				}
+				if !wrkld.IsDone() && expectedPk == tc.partitionOffset+tc.partitionCount {
+					t.Errorf("expected end of stream at %d", expectedPk)
+				}
+
+				if wrkld.IsDone() {
+					t.Log("got end of stream")
+					break
+				}
+
+				pk := wrkld.NextPartitionKey()
+				if pk != expectedPk {
+					t.Errorf("wrong PK; got %d; expected %d", pk, expectedPk)
+				} else {
+					t.Logf("got PK %d", pk)
+				}
+
+				ck := wrkld.NextClusteringKey()
+				if ck != expectedCk {
+					t.Errorf("wrong CK; got %d; expected %d", pk, expectedCk)
+				} else {
+					t.Logf("got CK %d", ck)
+				}
+
+				expectedCk++
+				if expectedCk == tc.clusteringRowCount {
+					if !wrkld.IsPartitionDone() {
+						t.Errorf("expected end of partition at %d", expectedCk)
+					} else {
+						t.Log("got end of partition")
+					}
+					expectedCk = 0
+					expectedPk++
+				} else {
+					if wrkld.IsPartitionDone() {
+						t.Errorf("got end of partition; expected %d", expectedCk)
+					}
+				}
+			}
+		})
+	}
+}
