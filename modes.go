3eb53d8ce11e5ad26062bcc662edb31dda521ccf
Botond Dénes <bdenes@scylladb.com>
2018-09-27 13:09:24 +0300

    Data validation: use random data and checksum instead of simple pattern
    The current data validation is quite simplistic and it depends on
    knowing the size of the clustering row beforehand. This latter is
    especially problematic since we want introduce support for random
    clustering row sizes.
    To prepare for this, data generation and validation is refactored.
    The data blob now consists of:
    * The size of the blob;
    * The primary key;
    * The clustering key;
    * A random payload;
    * A sha256 checksum calculated over the payload.
    
    This format allows for easily validating that:
    * The blob was not truncated or extended (stored size == actual size);
    * The blob comes from the partition and row it is expected to come from;
    * The blob was not changed. The checksum should be able to detect even a
      single bit flipped, while collisions (two rows having the same data)
      is practically impossible.
    
    It should be noted that only this "full format" is only possible to use
    when the size of the row is greater than a certain size (57 bytes,
    all mandatory fields + a single byte payload). When the size of the
    message is below this a special, deterministic format is used that has
    reduced capabilities of detecting corruption and avoiding collisions.
    
    It would also be quite simple to add a generation field and thus detect
    resurrected data.

--- modes.go
+++ modes.go
@@ -2,15 +2,18 @@ package main
 
 import (
 	"bytes"
+	"crypto/sha256"
+	"encoding/binary"
 	"fmt"
 	"log"
-	"strconv"
+	"math/rand"
 	"strings"
 	"sync/atomic"
 	"time"
 
 	"github.com/codahale/hdrhistogram"
 	"github.com/gocql/gocql"
+	"github.com/pkg/errors"
 )
 
 type RateLimiter interface {
@@ -287,23 +290,124 @@ func RunTest(resultChannel chan Result, workload WorkloadGenerator, rateLimiter 
 	resultChannel <- *rb.FullResult
 }
 
+const (
+	generatedDataHeaderSize int64 = 24
+	generatedDataMinSize int64 = generatedDataHeaderSize + 33
+)
+
 func GenerateData(pk int64, ck int64, size int64) []byte {
-	value := make([]byte, size)
-	if validateData {
-		dataPattern := strconv.FormatInt(pk*ck*(pk+ck), 10)
-		dataLen := len(dataPattern)
-		cnt := int(size) / dataLen
-		tail := int(size) % dataLen
-		var data string
-		if cnt > 0 {
-			data = strings.Repeat(dataPattern, cnt)
-		}
-		if tail > 0 {
-			data += dataPattern[:tail]
-		}
-		copy(value, []byte(data))
+	if !validateData {
+		return make([]byte, size);
 	}
+
+	buf := new(bytes.Buffer)
+
+	if size < generatedDataHeaderSize {
+		binary.Write(buf, binary.LittleEndian, int8(size))
+		binary.Write(buf, binary.LittleEndian, pk ^ ck)
+	} else {
+		binary.Write(buf, binary.LittleEndian, size)
+		binary.Write(buf, binary.LittleEndian, pk)
+		binary.Write(buf, binary.LittleEndian, ck)
+		if size < generatedDataMinSize {
+			for i := generatedDataHeaderSize; i < size; i++ {
+				binary.Write(buf, binary.LittleEndian, int8(0))
+			}
+		} else {
+			payload := make([]byte, size - generatedDataHeaderSize - sha256.Size)
+			rand.Read(payload)
+			csum := sha256.Sum256(payload)
+			binary.Write(buf, binary.LittleEndian, payload)
+			binary.Write(buf, binary.LittleEndian, csum)
+		}
+	}
+
+	value := make([]byte, size)
+	copy(value, buf.Bytes())
 	return value
+}
+
+func ValidateData(pk int64, ck int64, data []byte) error {
+	if !validateData {
+		return nil
+	}
+
+	buf := bytes.NewBuffer(data)
+	size := int64(buf.Len())
+
+	var storedSize int64
+	if size < generatedDataHeaderSize {
+		var storedSizeCompact int8
+		err := binary.Read(buf, binary.LittleEndian, &storedSizeCompact)
+		if err != nil {
+			return errors.Wrap(err, "failed to validate data, cannot read size from value")
+		}
+		storedSize = int64(storedSizeCompact)
+	} else {
+		err := binary.Read(buf, binary.LittleEndian, &storedSize)
+		if err != nil {
+			return errors.Wrap(err, "failed to validate data, cannot read size from value")
+		}
+	}
+
+	if size != storedSize {
+		return errors.Errorf("actual size of value (%d) doesn't match size stored in value (%d)", size, storedSize)
+	}
+
+	// There is no random payload for sizes < minFullSize
+	if size < generatedDataMinSize {
+		expectedBuf := GenerateData(pk, ck, size)
+		if !bytes.Equal(buf.Bytes(), expectedBuf) {
+			return errors.Errorf("actual value doesn't match expected value:\nexpected: %x\nactual: %x", expectedBuf, buf.Bytes())
+		}
+		return nil
+	}
+
+	var storedPk, storedCk int64
+	var err error
+
+	// Validate pk
+	err = binary.Read(buf, binary.LittleEndian, &storedPk)
+	if err != nil {
+		return errors.Wrap(err, "failed to validate data, cannot read pk from value")
+	}
+	if storedPk != pk {
+		return errors.Errorf("actual pk (%d) doesn't match pk stored in value (%d)", pk, storedPk)
+	}
+
+	// Validate ck
+	err = binary.Read(buf, binary.LittleEndian, &storedCk)
+	if err != nil {
+		return errors.Wrap(err, "failed to validate data, cannot read pk from value")
+	}
+	if storedCk != ck {
+		return errors.Errorf("actual ck (%d) doesn't match ck stored in value (%d)", ck, storedCk)
+	}
+
+	// Validate checksum over the payload
+	payload := make([]byte, size - generatedDataHeaderSize - sha256.Size)
+	err = binary.Read(buf, binary.LittleEndian, payload)
+	if err != nil {
+		return errors.Wrap(err, "failed to verify checksum, cannot read payload from value")
+	}
+
+	calculatedChecksumArray := sha256.Sum256(payload)
+	calculatedChecksum := calculatedChecksumArray[0:]
+
+	storedChecksum := make([]byte, 32)
+	err = binary.Read(buf, binary.LittleEndian, storedChecksum)
+	if err != nil {
+		return errors.Wrap(err, "failed to verify checksum, cannot read checksum from value")
+	}
+
+	if !bytes.Equal(calculatedChecksum, storedChecksum) {
+		return errors.New(fmt.Sprintf(
+			"corrupt checksum or data: calculated checksum (%x) doesn't match stored checksum (%x) over data\n%x",
+			calculatedChecksum,
+			storedChecksum,
+			payload))
+	}
+	return nil
 }
 
 func DoWrites(session *gocql.Session, resultChannel chan Result, workload WorkloadGenerator, rateLimiter RateLimiter) {
@@ -443,10 +547,10 @@ func DoReadsFromTable(table string, session *gocql.Session, resultChannel chan R
 			for iter.Scan(&resPk, &resCk, &value) {
 				rb.IncRows()
 				if validateData {
-					valueExpected := GenerateData(resPk, resCk, clusteringRowSize)
-					if bytes.Compare(value, valueExpected) != 0 {
+					err := ValidateData(resPk, resCk, value)
+					if err != nil {
 						rb.IncErrors()
-						log.Print("data corruption:", resPk, resCk, value, valueExpected)
+						log.Printf("data corruption in pk(%d), ck(%d): %s", resPk, resCk, err)
 					}
 				}
 			}
