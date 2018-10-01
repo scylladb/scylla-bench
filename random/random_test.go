fe7a377119ceb5952c5a169292aa0363ec525886
Botond Dénes <bdenes@scylladb.com>
2018-10-01 12:44:48 +0300

    random.ParseDistribution(): add support for : syntax
    '(' and ')' are special characters for bash and when used on the command
    line they either need to be escaped or quoted. Avoid this hassle for
    command-line users by providing an alternative syntax which separates
    the parameters from the distribution name by a `:` character.

--- random/random_test.go
+++ random/random_test.go
@@ -9,67 +9,77 @@ import (
 
 func TestParseDistribution(t *testing.T) {
 	cases := map[string]struct {
-		input string
-		want  random.Distribution
-		ok    bool
+		inputs []string
+		want   random.Distribution
+		ok     bool
 	}{
 		"fixed": {
-			`fixed(1)`,
+			[]string{`fixed(1)`, `fixed:1`},
 			&random.Fixed{Value: 1},
 			true,
 		},
 		"uniform": {
-			`uniform(1..10)`,
+			[]string{`uniform(1..10)`, `uniform:1..10`},
 			&random.Uniform{Min: 1, Max: 10},
 			true,
 		},
 		"invalid fixed #1": {
-			`fixed(asdfs)`,
+			[]string{`fixed(asdfs)`, `fixed:asdfs`},
 			nil,
 			false,
 		},
 		"invalid fixed #2": {
-			`fixed)1(`,
+			[]string{`fixed)1(`},
 			nil,
 			false,
 		},
 		"invalid fixed #3": {
-			`fixed()`,
+			[]string{`fixed()`, `fixed:`},
+			nil,
+			false,
+		},
+		"invalid fixed #4": {
+			[]string{`fixed:1)`},
 			nil,
 			false,
 		},
 		"invalid uniform #1": {
-			`uniform(1..)`,
+			[]string{`uniform(1..)`, `uniform:1..`},
 			nil,
 			false,
 		},
 		"invalid uniform #2": {
-			`uniform(1..)`,
+			[]string{`uniform(..)`, `uniform:..`},
 			nil,
 			false,
 		},
 		"invalid uniform #3": {
-			`uniform(10..abc)`,
+			[]string{`uniform(10..abc)`, `uniform:10..abc`},
 			nil,
 			false,
 		},
 		"invalid uniform #4": {
-			`uniform(abc..def)`,
+			[]string{`uniform(abc..def)`, `uniform:abc..def`},
 			nil,
 			false,
 		},
 		"invalid uniform #5": {
-			`uniform(300..100)`,
+			[]string{`uniform(300..100)`, `uniform:300..100`},
+			nil,
+			false,
+		},
+		"invalid uniform #6": {
+			[]string{`uniform:10..100)`},
 			nil,
 			false,
 		},
 		"unsupported #1": {
-			`unsupported(1..100, 10)`,
+			[]string{`unsupported(1..100, 10)`, `unsupported:1..100,10`},
 			nil,
 			false,
 		},
 		"unsupported #2": {
-			`~fixed(1..100, 10)`,
+			[]string{`~fixed(1..100, 10)`, `~fixed:1..100,10`},
 			nil,
 			false,
 		},
@@ -77,18 +87,20 @@ func TestParseDistribution(t *testing.T) {
 
 	for name, cas := range cases {
 		t.Run(name, func(t *testing.T) {
-			got, err := random.ParseDistribution(cas.input)
-			if err != nil {
-				if !cas.ok {
-					return
+			for _, input := range cas.inputs {
+				got, err := random.ParseDistribution(input)
+				if err != nil {
+					if !cas.ok {
+						return
+					}
+					t.Fatalf("ParseDistribution(%q)=%s", input, err)
 				}
-				t.Fatalf("ParseDistribution(%q)=%s", cas.input, err)
-			}
-			if !cas.ok {
-				t.Errorf("expected ParseDistribution(%q) to fail", cas.input)
-			}
-			if !reflect.DeepEqual(got, cas.want) {
-				t.Errorf("got %+v, want %+v", got, cas.want)
+				if !cas.ok {
+					t.Errorf("expected ParseDistribution(%q) to fail", input)
+				}
+				if !reflect.DeepEqual(got, cas.want) {
+					t.Errorf("got %+v, want %+v", got, cas.want)
+				}
 			}
 		})
 	}
@@ -96,32 +108,34 @@ func TestParseDistribution(t *testing.T) {
 
 func TestParseRatio(t *testing.T) {
 	cases := map[string]struct {
-		input string
-		want  int64
-		ok    bool
+		inputs []string
+		want   int64
+		ok     bool
 	}{
-		"fixed":       {`fixed(1)/1`, 1, true},
-		"uniform":     {`uniform(1..10)/10`, 10, true},
-		"unsupported": {`unsupported(1..10, 10)/10`, 0, false},
-		"invalid #1":  {`fixed(1)/`, 0, false},
-		"invalid #2":  {`fixed(1)/abc`, 0, false},
-		"invalid #3":  {`fixed(1)/0`, 0, false},
+		"fixed":       {[]string{`fixed(1)/1`, `fixed:1/1`}, 1, true},
+		"uniform":     {[]string{`uniform(1..10)/10`, `uniform:1..10/10`}, 10, true},
+		"unsupported": {[]string{`unsupported(1..10, 10)/10`, `unsupported:1..10,10/10`}, 0, false},
+		"invalid #1":  {[]string{`fixed(1)/`, `fixed:1/`}, 0, false},
+		"invalid #2":  {[]string{`fixed(1)/abc`, `fixed:1/abc`}, 0, false},
+		"invalid #3":  {[]string{`fixed(1)/0`, `fixed:1/0`}, 0, false},
 	}
 
 	for name, cas := range cases {
 		t.Run(name, func(t *testing.T) {
-			got, err := random.ParseRatio(cas.input)
-			if err != nil {
-				if !cas.ok {
-					return
+			for _, input := range cas.inputs {
+				got, err := random.ParseRatio(input)
+				if err != nil {
+					if !cas.ok {
+						return
+					}
+					t.Fatalf("ParseRatio(%q)=%s", input, err)
 				}
-				t.Fatalf("ParseRatio(%q)=%s", cas.input, err)
-			}
-			if !cas.ok {
-				t.Errorf("expected ParseRatio(%q) to fail", cas.input)
-			}
-			if got.Value != cas.want {
-				t.Errorf("got %d, want %d", got.Value, cas.want)
+				if !cas.ok {
+					t.Errorf("expected ParseRatio(%q) to fail", input)
+				}
+				if got.Value != cas.want {
+					t.Errorf("got %d, want %d", got.Value, cas.want)
+				}
 			}
 		})
 	}
