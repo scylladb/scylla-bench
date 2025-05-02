scylladb/scylla-bench

commit 016b539fe0889e3a36d8278688e8edd550584c03

fix(random-string): generate random strings using math/rand/v2 package
Signed-off-by: Dusan Malusev <dusan.malusev@scylladb.dev>

modes.go
@@ -22,6 +22,7 @@ import (
 	"github.com/scylladb/scylla-bench/pkg/results"
 	//nolint
 	. "github.com/scylladb/scylla-bench/pkg/workloads"
+	"github.com/scylladb/scylla-bench/random"
 )
 
 const reportInterval time.Duration = 1 * time.Second
@@ -257,11 +258,9 @@ func GenerateData(pk, ck, size int64) ([]byte, error) {
 		return buf.Bytes(), nil
 	}
 
-	seed := sha256.Sum256([]byte(strconv.FormatUint(rand.Uint64(), 10)))
-	reader := rand.NewChaCha8(seed)
-
 	payload := buf.AvailableBuffer()
-	_, _ = reader.Read(payload[:size-sha256.Size])
+	_, _ = random.String(payload[:size-sha256.Size])
+
 	if err := binary.Write(&buf, binary.LittleEndian, payload); err != nil {
 		return nil, err
 	}
@@ -314,11 +313,10 @@ func ValidateData(pk, ck int64, data []byte) error {
 	}
 
 	var storedPk, storedCk int64
-	var err error
 
 	// Validate pk
-	err = binary.Read(buf, binary.LittleEndian, &storedPk)
-	if err != nil {
+
+	if err := binary.Read(buf, binary.LittleEndian, &storedPk); err != nil {
 		return errors.Wrap(err, "failed to validate data, cannot read pk from value")
 	}
 	if storedPk != pk {
@@ -326,8 +324,7 @@ func ValidateData(pk, ck int64, data []byte) error {
 	}
 
 	// Validate ck
-	err = binary.Read(buf, binary.LittleEndian, &storedCk)
-	if err != nil {
+	if err := binary.Read(buf, binary.LittleEndian, &storedCk); err != nil {
 		return errors.Wrap(err, "failed to validate data, cannot read pk from value")
 	}
 	if storedCk != ck {
@@ -336,21 +333,21 @@ func ValidateData(pk, ck int64, data []byte) error {
 
 	// Validate checksum over the payload
 	payload := make([]byte, size-generatedDataHeaderSize-sha256.Size)
-	err = binary.Read(buf, binary.LittleEndian, payload)
-	if err != nil {
+
+	if err := binary.Read(buf, binary.LittleEndian, payload); err != nil {
 		return errors.Wrap(err, "failed to verify checksum, cannot read payload from value")
 	}
 
 	calculatedChecksumArray := sha256.Sum256(payload)
 	calculatedChecksum := calculatedChecksumArray[0:]
 
-	storedChecksum := make([]byte, 32)
-	err = binary.Read(buf, binary.LittleEndian, storedChecksum)
-	if err != nil {
+	var storedChecksum [sha256.Size]byte
+
+	if err := binary.Read(buf, binary.LittleEndian, storedChecksum); err != nil {
 		return errors.Wrap(err, "failed to verify checksum, cannot read checksum from value")
 	}
 
-	if !bytes.Equal(calculatedChecksum, storedChecksum) {
+	if !bytes.Equal(calculatedChecksum, storedChecksum[:]) {
 		return fmt.Errorf(
 			"corrupt checksum or data: calculated checksum (%x) doesn't match stored checksum (%x) over data\n%x",
 			calculatedChecksum,
@@ -378,23 +375,23 @@ func DoWrites(session *gocql.Session, threadResult *results.TestThreadResult, wo
 	RunTest(threadResult, workload, rateLimiter, func(rb *results.TestThreadResult) (error, time.Duration) {
 		request := "INSERT INTO " + keyspaceName + "." + tableName + " (pk, ck, v) VALUES (?, ?, ?)"
 		query := session.Query(request)
+		defer query.Release()
 		pk := workload.NextPartitionKey()
 		ck := workload.NextClusteringKey()
 		value, err := GenerateData(pk, ck, clusteringRowSizeDist.Generate())
 		if err != nil {
 			panic(err)
 		}
-		bound := query.Bind(pk, ck, value)
 
 		currentAttempts := 0
 		// NOTE: use custom query string instead of 'query.String()' to avoid huge values printings
 		queryStr := fmt.Sprintf(
 			"[query statement=%q values=%+v consistency=%s]",
-			request, []interface{}{pk, ck, "<" + strconv.Itoa(len(value)) + "-bytes-value>"},
+			request, []any{pk, ck, "<" + strconv.Itoa(len(value)) + "-bytes-value>"},
 			query.GetConsistency())
 		for {
 			requestStart := time.Now()
-			err := bound.Exec()
+			err = query.Exec()
 			requestEnd := time.Now()
 
 			if err == nil {
@@ -475,15 +472,16 @@ func DoBatchedWrites(session *gocql.Session, threadResult *results.TestThreadRes
 
 func DoCounterUpdates(session *gocql.Session, threadResult *results.TestThreadResult, workload WorkloadGenerator, rateLimiter RateLimiter) {
 	RunTest(threadResult, workload, rateLimiter, func(rb *results.TestThreadResult) (error, time.Duration) {
-		query := session.Query("UPDATE " + keyspaceName + "." + counterTableName +
-			" SET c1 = c1 + ?, c2 = c2 + ?, c3 = c3 + ?, c4 = c4 + ?, c5 = c5 + ? WHERE pk = ? AND ck = ?")
 		pk := workload.NextPartitionKey()
 		ck := workload.NextClusteringKey()
-		bound := query.Bind(ck, ck+1, ck+2, ck+3, ck+4, pk, ck)
+
+		query := session.Query("UPDATE "+keyspaceName+"."+counterTableName+
+			" SET c1 = c1 + ?, c2 = c2 + ?, c3 = c3 + ?, c4 = c4 + ?, c5 = c5 + ? WHERE pk = ? AND ck = ?").
+			Bind(ck, ck+1, ck+2, ck+3, ck+4, pk, ck)
 		currentAttempts := 0
 		for {
 			requestStart := time.Now()
-			err := bound.Exec()
+			err := query.Exec()
 			requestEnd := time.Now()
 
 			if err == nil {
@@ -549,27 +547,26 @@ func DoReadsFromTable(table string, session *gocql.Session, threadResult *result
 		pk := workload.NextPartitionKey()
 		query := BuildReadQuery(table, selectOrderByParsed[counter%numOfOrderings], session)
 
-		var bound *gocql.Query
 		switch {
 		case inRestriction:
-			args := make([]interface{}, 1, rowsPerRequest+1)
+			args := make([]any, 1, rowsPerRequest+1)
 			args[0] = pk
-			for i := 0; i < rowsPerRequest; i++ {
+			for range rowsPerRequest {
 				if workload.IsPartitionDone() {
 					args = append(args, 0)
 				} else {
 					args = append(args, workload.NextClusteringKey())
 				}
 			}
-			bound = query.Bind(args...)
+			query.Bind(args...)
 		case noLowerBound:
-			bound = query.Bind(pk)
+			query.Bind(pk)
 		default:
 			ck := workload.NextClusteringKey()
 			if provideUpperBound {
-				bound = query.Bind(pk, ck, ck+int64(rowsPerRequest))
+				query.Bind(pk, ck, ck+int64(rowsPerRequest))
 			} else {
-				bound = query.Bind(pk, ck)
+				query.Bind(pk, ck)
 			}
 		}
 
@@ -579,7 +576,7 @@ func DoReadsFromTable(table string, session *gocql.Session, threadResult *result
 		currentAttempts := 0
 		for {
 			requestStart := time.Now()
-			iter := bound.Iter()
+			iter := query.Iter()
 			if table == tableName {
 				for iter.Scan(&resPk, &resCk, &value) {
 					rb.IncRows()

Makefile
@@ -62,7 +62,7 @@ fmt:
 
 .PHONY: test
 test:
-	@go test -covermode=atomic -race -coverprofile=coverage.txt -timeout 5m -json -v ./... 2>&1 | gotestfmt -showteststatus
+	@go test -covermode=atomic -race -coverprofile=coverage.txt -timeout 5m -json -v ./... 2>&1 | go tool gotestfmt -showteststatus
 
 .PHONY: check
 check:

go.mod
@@ -82,6 +82,7 @@ require (
 	github.com/gostaticanalysis/comment v1.5.0 // indirect
 	github.com/gostaticanalysis/forcetypeassert v0.2.0 // indirect
 	github.com/gostaticanalysis/nilerr v0.1.1 // indirect
+	github.com/gotesttools/gotestfmt/v2 v2.5.0 // indirect
 	github.com/hashicorp/go-immutable-radix/v2 v2.1.0 // indirect
 	github.com/hashicorp/go-version v1.7.0 // indirect
 	github.com/hashicorp/golang-lru/v2 v2.0.7 // indirect
@@ -205,6 +206,7 @@ replace github.com/gocql/gocql => github.com/scylladb/gocql v1.14.5
 
 tool (
 	github.com/golangci/golangci-lint/cmd/golangci-lint
+	github.com/gotesttools/gotestfmt/v2/cmd/gotestfmt
 	golang.org/x/tools/go/analysis/passes/fieldalignment/cmd/fieldalignment
 	mvdan.cc/gofumpt
 )

go.sum
@@ -302,6 +302,8 @@ github.com/gostaticanalysis/nilerr v0.1.1/go.mod h1:wZYb6YI5YAxxq0i1+VJbY0s2YONW
 github.com/gostaticanalysis/testutil v0.3.1-0.20210208050101-bfb5c8eec0e4/go.mod h1:D+FIZ+7OahH3ePw/izIEeH5I06eKs1IKI4Xr64/Am3M=
 github.com/gostaticanalysis/testutil v0.5.0 h1:Dq4wT1DdTwTGCQQv3rl3IvD5Ld0E6HiY+3Zh0sUGqw8=
 github.com/gostaticanalysis/testutil v0.5.0/go.mod h1:OLQSbuM6zw2EvCcXTz1lVq5unyoNft372msDY0nY5Hs=
+github.com/gotesttools/gotestfmt/v2 v2.5.0 h1:fSU3MnR+E+fvuXdw1l8xbufKhDxY3Tfjsjx/I1WerB4=
+github.com/gotesttools/gotestfmt/v2 v2.5.0/go.mod h1:oQJg2KZ2aGoqEbMC2PDaAeBYm0tOkocgixK9FzsCdp4=
 github.com/hailocab/go-hostpool v0.0.0-20160125115350-e80d13ce29ed h1:5upAirOpQc1Q53c0bnx2ufif5kANL7bfZWcc6VJWJd8=
 github.com/hailocab/go-hostpool v0.0.0-20160125115350-e80d13ce29ed/go.mod h1:tMWxXQ9wFIaZeTI9F+hmhFiGpFmhOHzyShyFUhRm0H4=
 github.com/hashicorp/go-immutable-radix/v2 v2.1.0 h1:CUW5RYIcysz+D3B+l1mDeXrQ7fUvGGCwJfdASSzbrfo=

random/random.go
@@ -1,6 +1,7 @@
 package random
 
 import (
+	"crypto/sha256"
 	"encoding/binary"
 	"encoding/hex"
 	"fmt"
@@ -29,10 +30,16 @@ var (
 )
 
 var (
+	globalSeed = uint64(time.Now().UnixNano() + int64(os.Getpid()))
 	// globalRand is a copy of the rand.globalRand (git.io/fA2Ls) for
 	// user mode package's use only.
-	globalRand = NewLockedRandom(uint64(time.Now().UnixNano() + int64(os.Getpid())))
+	globalRand    = NewLockedRandom(globalSeed)
+	globalRandStr = NewLockedRandomString(int64(globalSeed))
 )
+
+func String(data []byte) (int, error) {
+	return globalRandStr.Read(data)
+}
 
 // Product gives a product of values generated by the given distributions.
 // The smallest value returned by the function is 1.
@@ -280,7 +287,7 @@ func (u Uniform) Generate() int64 {
 
 type LockedRandom struct {
 	src *rand.Rand
-	mu  sync.RWMutex
+	mu  sync.Mutex
 }
 
 func NewLockedRandom(seed uint64) *LockedRandom {
@@ -290,9 +297,9 @@ func NewLockedRandom(seed uint64) *LockedRandom {
 }
 
 func (r *LockedRandom) Int64N(maximum int64) (n int64) {
-	r.mu.RLock()
+	r.mu.Lock()
 	n = r.src.Int64N(maximum)
-	r.mu.RUnlock()
+	r.mu.Unlock()
 	return
 }
 
@@ -300,4 +307,31 @@ func (r *LockedRandom) Seed(seed int64) {
 	r.mu.Lock()
 	r.src = rand.New(rand.NewPCG(uint64(seed), uint64(seed)))
 	r.mu.Unlock()
+}
+
+type LockedRandomString struct {
+	rnd *rand.ChaCha8
+	mu  sync.Mutex
+}
+
+func NewLockedRandomString(seed int64) *LockedRandomString {
+	l := &LockedRandomString{}
+	l.Seed(seed)
+
+	return l
+}
+
+func (r *LockedRandomString) Seed(seed int64) {
+	s := sha256.Sum256([]byte(strconv.FormatUint(uint64(seed), 10)))
+
+	r.mu.Lock()
+	r.rnd = rand.NewChaCha8(s)
+	r.mu.Unlock()
+}
+
+func (r *LockedRandomString) Read(b []byte) (n int, err error) {
+	r.mu.Lock()
+	n, err = r.rnd.Read(b)
+	defer r.mu.Unlock()
+	return
 }

random/random_test.go
@@ -1,7 +1,9 @@
 package random_test
 
 import (
+	"math"
 	"reflect"
+	"strconv"
 	"testing"
 
 	"github.com/scylladb/scylla-bench/random"
@@ -136,6 +138,64 @@ func TestParseRatio(t *testing.T) {
 				if got.Value != cas.want {
 					t.Errorf("got %d, want %d", got.Value, cas.want)
 				}
+			}
+		})
+	}
+}
+
+func TestRandomString(t *testing.T) {
+	t.Parallel()
+
+	rnd := random.NewLockedRandomString(100)
+
+	for _, size := range []int{128, 256, 512, 1024, 8192} {
+		t.Run("generate_"+strconv.FormatInt(int64(size), 10), func(t *testing.T) {
+			t.Parallel()
+
+			generated := make(map[string]struct{}, 1000)
+			same := 0
+			for range 1000 {
+				data := make([]byte, size)
+				_, _ = rnd.Read(data)
+				if _, ok := generated[string(data)]; ok {
+					same++
+				} else {
+					generated[string(data)] = struct{}{}
+				}
+			}
+
+			if same > 10 {
+				t.Errorf("generated %d same strings of size %d", same, size)
+			}
+		})
+	}
+}
+
+func BenchmarkNewLockedRandom(b *testing.B) {
+	b.ReportAllocs()
+
+	rnd := random.NewLockedRandom(100)
+	b.ResetTimer()
+
+	for b.Loop() {
+		_ = rnd.Int64N(math.MaxInt64)
+	}
+}
+
+func BenchmarkNewLockedRandomString(b *testing.B) {
+	b.ReportAllocs()
+	rnd := random.NewLockedRandomString(100)
+	b.ResetTimer()
+
+	sizes := []int{1, 10, 100, 1024, 8192}
+
+	for _, size := range sizes {
+		b.Run("generate_"+strconv.FormatInt(int64(size), 10), func(b *testing.B) {
+			b.ResetTimer()
+			b.ReportAllocs()
+			data := make([]byte, size)
+			for b.Loop() {
+				_, _ = rnd.Read(data)
 			}
 		})
 	}

commit 2eff67e36c67cf9a26943d45e15777e3a7dd5a62

Fix panic in DoWrite
Regression since 016b539f
query.Bind is not performed which leads to panic:
```
panic: runtime error: index out of range [0] with length 0
github.com/gocql/gocql.createRoutingKey(0xc0002a0000, {0x0, 0x0, 0x0})
	/home/dmitry.kropachev/go/pkg/mod/github.com/scylladb/gocql@v1.14.6-0.20250502134006-32ec722fb53b/session.go:2294 +0x67f
github.com/gocql/gocql.(*Query).GetRoutingKey(0xc0003a8500)
```

modes.go
@@ -382,6 +382,7 @@ func DoWrites(session *gocql.Session, threadResult *results.TestThreadResult, wo
 		if err != nil {
 			panic(err)
 		}
+		bound := query.Bind(pk, ck, value)
 
 		currentAttempts := 0
 		// NOTE: use custom query string instead of 'query.String()' to avoid huge values printings
@@ -391,7 +392,7 @@ func DoWrites(session *gocql.Session, threadResult *results.TestThreadResult, wo
 			query.GetConsistency())
 		for {
 			requestStart := time.Now()
-			err = query.Exec()
+			err = bound.Exec()
 			requestEnd := time.Now()
 
 			if err == nil {